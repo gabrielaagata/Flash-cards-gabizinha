0ebf64180ba17787089bc99e08a5b156529683be
gabrielaagata <145785076+gabrielaagata@users.noreply.github.com>
2024-10-30 09:44:54 -0300

    30/10

--- aap.js
+++ aap.js
@@ -14,5 +14,19 @@ function criaCartao(categoria, pergunta, resposta){
                     </div>
                 </div>
   `
+
+  let respostaEstaVisivel = false;
+
+  function viraCartao(){
+    respostaEstaVisivel = !respostaEstaVisivel;
+    cartao.classList.toggle('active', respostaEstaVisivel);
+  }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
   container.appendChild(cartao);
 }
