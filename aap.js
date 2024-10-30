814e92a5be1d082b6f79691ef5c5dd59b8cf59c7
gabrielaagata <145785076+gabrielaagata@users.noreply.github.com>
2024-10-30 10:13:43 -0300

    30/10

--- aap.js
+++ aap.js
@@ -1,9 +1,9 @@
-function criaCartao(categoria, pergunta, resposta){
-  let container = document.getElementById('container');
-  let cartao = document.createElement('article');
-  cartao.className = 'cartao';
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
 
-  cartao.innerHTML = `
+    cartao.innerHTML = `
   <div class="cartao__conteudo">
                     <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
@@ -15,18 +15,14 @@ function criaCartao(categoria, pergunta, resposta){
                 </div>
   `
 
-  let respostaEstaVisivel = false;
+    let respostaEstaVisivel = false;
 
-  function viraCartao(){
-    respostaEstaVisivel = !respostaEstaVisivel;
-    cartao.classList.toggle('active', respostaEstaVisivel);
-  }
- 
- 
- 
- 
- 
- 
- 
-  container.appendChild(cartao);
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
+    }
+
+    cartao.addEventListener('click', viraCartao);
+
+    container.appendChild(cartao);
 }
